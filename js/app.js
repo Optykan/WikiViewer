5c581c31625185aab3b0c70040b50184a1aefc9c
Optykan <syang0525@gmail.com>
2016-06-13 21:44:50 -0400

    my code doesn't work and I dont know why:

--- js/app.js
+++ js/app.js
@@ -3,25 +3,36 @@
 	'use strict';
 	
 	var WikiInterface={
-
 		initialize: function(){
+			var that=this;
 			console.log("WikiInterface initialized");
+			$("#search").keypress(function(e){
+				if(e.which==13){
+					that.query(this.value);
+				}
+			});
 		},
-		handler: function(data, status){
+		handler: function(data){
 			console.log(data);
 		},
 		query: function(q){
+			var that=this;
 			$.ajax({
+				method: "GET",
 				url: "https://en.wikipedia.org/w/api.php",
 				data:{
 					"action": "opensearch",
 					"format": "json",
-					"search": q
+					"search": q,
 				},
-				success: this.handler
+				error: function(xhr, s, e){
+					console.log(xhr);
+					console.log(s);
+					console.log(e);
+				},
+				success: that.handler
 			});
 		}
-
 	};
 
 	WikiInterface.initialize();
