5d70a812aa13f5dc1b38fe3e3b619e2faccc93e1
Optykan <syang0525@gmail.com>
2016-06-14 18:58:27 -0400

    Data type as JSONP

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 	'use strict';
 	
 	var WikiInterface={
+		results: [],
 		initialize: function(){
 			var that=this;
 			console.log("WikiInterface initialized");
@@ -13,24 +14,34 @@
 			});
 		},
 		handler: function(data){
-			console.log(data);
+			for(let i=0; i<data[1].length; i++){
+				this.results.push({
+					"title": data[1][i],
+					"snippet": data[2][i],
+					"url": data[3][i]
+				});
+			}
+			console.log(this.results);
 		},
 		query: function(q){
 			var that=this;
 			$.ajax({
 				method: "GET",
 				url: "https://en.wikipedia.org/w/api.php",
+				dataType: "jsonp",
 				data:{
 					"action": "opensearch",
 					"format": "json",
 					"search": q,
 				},
-				error: function(xhr, s, e){
-					console.log(xhr);
-					console.log(s);
-					console.log(e);
+				headers: {
+					'Api-User-Agent': 'WikiViewer/0.1',
+					'Accept': 'application/json'
 				},
-				success: that.handler
+				error: function(xhr, s, e){
+					console.log("An error occurred.");
+				},
+				success: that.handler.bind(that) //bind the context of the handler to the WikiViewer object, not the ajax object
 			});
 		}
 	};
